49bc961a144f3700efaf9daa71dae6bf61e23385
ulwfcyvi791837060 <1632745162@qq.com>
2019-06-23 09:28:17 +0800

    no message

---
+++ aiom/src/main/java/com/yyx/aio/task/util/GetFileName.java
@@ -0,0 +1,47 @@
+package com.yyx.aio.task.util;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class GetFileName
+{
+    public static String [] getFileName(String path)
+    {
+        File file = new File(path);
+        String [] fileName = file.list();
+        return fileName;
+    }
+    public static void getAllFileName(String path, ArrayList<String> fileName)
+    {
+        File file = new File(path);
+        File [] files = file.listFiles();
+        String [] names = file.list();
+        if(names != null)
+        fileName.addAll(Arrays.asList(names));
+        for(File a:files)
+        {
+            if(a.isDirectory())
+            {
+                getAllFileName(a.getAbsolutePath(),fileName);
+            }
+        }
+    }
+    public static void main(String[] args)
+    {
+        String [] fileName = getFileName("I:\\ulwfcyvi\\java相关\\java视频教程");
+        for(String name:fileName)
+        {
+            System.out.println(name);
+        }
+
+        /*System.out.println("--------------------------------");
+        ArrayList<String> listFileName = new ArrayList<String>(); 
+        getAllFileName("F:\\xiaoshuo",listFileName);
+        for(String name:listFileName)
+        {
+            System.out.println(name);
+        }*/
+         
+    }
+}
